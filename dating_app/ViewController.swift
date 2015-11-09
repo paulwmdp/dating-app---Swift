e65a3c910d829175462fc20f43d81b3030d16a33
TakeumiYamamura <yama1223xxx@gmail.com>
2015-11-09 14:24:11 +0900

    first commit

---
+++ dating_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dating_app
+//
+//  Created by TakefumiYamamura on 2015/11/09.
+//  Copyright (c) 2015年 ids. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ dating_appTests/dating_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  dating_appTests.swift
+//  dating_appTests
+//
+//  Created by TakefumiYamamura on 2015/11/09.
+//  Copyright (c) 2015年 ids. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class dating_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
